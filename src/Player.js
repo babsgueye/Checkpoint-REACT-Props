86fcd460c42f46f14f703e5e2cff429a9ab0b175
Babacar GUEYE <gueyebabacar45@gmail.com>
2023-07-25 17:15:40 +0000

    first commit

---
+++ src/Player.js
@@ -0,0 +1,24 @@
+import Card from 'react-bootstrap/Card';
+import players from './players';
+
+const Player = ({imageUrl,name,team,nationality,jerseyNumber,age}) => {
+     return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={imageUrl} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          Team:{team}
+          <br/>
+          Nationality:{nationality}
+          <br/>
+          Jersey Number:{jerseyNumber}
+          <br/>
+          Age:{age}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Player;
---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players = [
+    {
+        imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/2022_FIFA_World_Cup_France_4%E2%80%931_Australia_-_%287%29_%28cropped%29.jpg/250px-2022_FIFA_World_Cup_France_4%E2%80%931_Australia_-_%287%29_%28cropped%29.jpg",
+        name:"Kylian Mbappe",
+        team:"PSG",
+        nationality:"France",
+        jerseyNumber:7,
+        age:24,
+    },
+    {
+        imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg/250px-Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg",
+        name:"Lionel Messi",
+        team:"Inter Miami CF",
+        nationality:"Argentine",
+        jerseyNumber:10,
+        age:36, 
+    },
+    {
+        imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Sadio_Mane_N%2A10_du_Senegal_%28cropped%29.jpg/220px-Sadio_Mane_N%2A10_du_Senegal_%28cropped%29.jpg",
+        name:"Sadio Mane",
+        team:"Bayern Munich",
+        nationality:"Senegal",
+        jerseyNumber:17,
+        age:31, 
+    },
+    {
+        imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Joueur_de_foot.jpg/250px-Joueur_de_foot.jpg",
+        name:"Kalidou Koulibaly",
+        team:"Al-Hilal FC",
+        nationality:"Senegal",
+        jerseyNumber:3,
+        age:32,  
+    },
+];
+
+export default players;
